5d6861d0bec8c0a392bd3f2fc5b1efd8cb58bc65
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2017-10-03 19:04:26 -0700

    Remove env test

--- PowerShell/BuildingBlocks/CustomerAndResources/AssignSubscriptions.ps1
+++ PowerShell/BuildingBlocks/CustomerAndResources/AssignSubscriptions.ps1
@@ -2,10 +2,10 @@
 .\Init.ps1 -Customer
 
 # Get the end users under that customer (this example retrieves the top 100)
-$customerEndUsers =  Get-BT_CustomerEndUser -Ticket $mspc.CustomerTicket -OrganizationId $mspc.Customer.OrganizationId -IsDeleted $False -PageSize 100 -Env Test
+$customerEndUsers =  Get-BT_CustomerEndUser -Ticket $mspc.CustomerTicket -OrganizationId $mspc.Customer.OrganizationId -IsDeleted $False -PageSize 100
 if ( $customerEndUsers ) {
     # Check for existing subscriptions
-    $existingSubscriptions = Get-BT_Subscription -Ticket $mspc.WorkgroupTicket -SubscriptionEntityReferenceType "CustomerEndUser" -EntityReferenceId $customerEndUsers.Id -IsDeleted $False -Env Test
+    $existingSubscriptions = Get-BT_Subscription -Ticket $mspc.WorkgroupTicket -SubscriptionEntityReferenceType "CustomerEndUser" -EntityReferenceId $customerEndUsers.Id -IsDeleted $False
     
     # Filter out end users who already have a subscription
     $customerEndUsersToSubscribe = $customerEndUsers | Where {
@@ -13,10 +13,10 @@ if ( $customerEndUsers ) {
     }
     
     # Get the product sku id for the MSPC yearly subscription
-    $productSkuId = Get-BT_ProductSkuId -Ticket $mspc.Ticket -ProductName MspcEndUserYearlySubscription -Env Test
+    $productSkuId = Get-BT_ProductSkuId -Ticket $mspc.Ticket -ProductName MspcEndUserYearlySubscription
 
     # Assign subscriptions to each customer end user
     $customerEndUsersToSubscribe | ForEach {
-        Add-BT_Subscription -Ticket $mspc.WorkgroupTicket -SubscriptionEntityReferenceType CustomerEndUser -EntityReferenceId $_.Id -ProductSkuId $productSkuId -Env Test  
+        Add-BT_Subscription -Ticket $mspc.WorkgroupTicket -SubscriptionEntityReferenceType CustomerEndUser -EntityReferenceId $_.Id -ProductSkuId $productSkuId 
     }
 }
