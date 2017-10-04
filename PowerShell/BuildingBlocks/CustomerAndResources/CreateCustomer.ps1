7285fec0dd5429d8f6fb8ba11cac466982b2e727
Lee Soe <LSoe@BitTitan.com>
2017-10-03 18:08:38 -0700

    Add CreateCustomer and DeployService building blocks.

---
+++ PowerShell/BuildingBlocks/CustomerAndResources/CreateCustomer.ps1
@@ -0,0 +1,23 @@
+﻿param(
+  [Parameter(Mandatory=$true)]
+  [string]$PrimaryDomain,
+  [Parameter(Mandatory=$true)]
+  [string]$CompanyName,
+  [Parameter(Mandatory=$false)]
+  [string]$CountryName = "")
+
+# Initialize the session
+.\Init.ps1
+
+# Check if a customer with the same domain already exists
+$existingCustomer = Get-BT_Customer -Ticket $mspc.Ticket -WorkgroupId $mspc.Workgroup.Id -PrimaryDomain $PrimaryDomain
+if ( $existingCustomer -ne $null ) {
+    # Write a warning if a customer already exists
+    Write-warning "Customer with primary domain $primaryDomain already exists. A new customer was not created."
+}
+else {
+    # Add a new customer
+    # Additional parameters can be set such as CityName, IndustryType, CompanySize and more
+    # Use Get-Help Add-BT_Customer to see all parameters
+    Add-BT_Customer -Ticket $mspc.Ticket -WorkgroupId $mspc.Workgroup.Id -PrimaryDomain $PrimaryDomain -CompanyName $CompanyName -CountryName $CountryName
+}
---
+++ PowerShell/BuildingBlocks/ServicesAndTasks/DeployService.ps1
@@ -0,0 +1,13 @@
+﻿param(
+  [Parameter(Mandatory=$true)]
+  [string]$ServiceKeyName)
+
+# Initialize the session
+.\Init.ps1 -Customer
+
+# Find the service with the specified name
+$offeringMetadata = Get-BT_OfferingMetadata -Ticket $mspc.Ticket -KeyName $ServiceKeyName
+
+# Deploy the service
+Add-BT_OfferingInstance -Ticket $mspc.CustomerTicket -OfferingMetadataId $offeringMetadata.Id
+Write-Verbose "Deployed service: $ServiceKeyName."
