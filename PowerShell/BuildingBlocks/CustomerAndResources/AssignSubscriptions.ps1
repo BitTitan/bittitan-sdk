56c9fe87116482f24dfc7341842eec4d586d3ea9
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2017-10-04 11:41:07 -0700

    Address pull request feedback

--- PowerShell/BuildingBlocks/CustomerAndResources/AssignSubscriptions.ps1
+++ PowerShell/BuildingBlocks/CustomerAndResources/AssignSubscriptions.ps1
@@ -2,14 +2,14 @@
 .\Init.ps1 -Customer
 
 # Get the end users under that customer (this example retrieves the top 100)
-$customerEndUsers =  Get-BT_CustomerEndUser -Ticket $mspc.CustomerTicket -OrganizationId $mspc.Customer.OrganizationId -IsDeleted $False -PageSize 100
+$customerEndUsers =  Get-BT_CustomerEndUser -Ticket $mspc.CustomerTicket -IsDeleted $False -PageSize 100
 if ( $customerEndUsers ) {
     # Check for existing subscriptions
     $existingSubscriptions = Get-BT_Subscription -Ticket $mspc.WorkgroupTicket -SubscriptionEntityReferenceType CustomerEndUser -EntityReferenceId $customerEndUsers.Id -IsDeleted $False
     
     # Filter out end users who already have a subscription
     $customerEndUsersToSubscribe = $customerEndUsers | Where {
-        !( $existingSubscriptions.EntityReferenceId -contains $_.Id ) 
+        $existingSubscriptions.EntityReferenceId -notcontains $_.Id 
     }
     
     # Get the product sku id for the MSPC yearly subscription
@@ -19,4 +19,6 @@ if ( $customerEndUsers ) {
     $customerEndUsersToSubscribe | ForEach {
         Add-BT_Subscription -Ticket $mspc.WorkgroupTicket -SubscriptionEntityReferenceType CustomerEndUser -EntityReferenceId $_.Id -ProductSkuId $productSkuId 
     }
+    $assignedSubscriptionCount = $customerEndUsersToSubscribe.Length
+    Write-Verbose "Successfully assigned subscription to $assignedSubscriptionCount end users."
 }
