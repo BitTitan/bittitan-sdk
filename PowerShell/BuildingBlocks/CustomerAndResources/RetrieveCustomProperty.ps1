0a83c5d46d23a7a5a05fd62c8c340df74e3fa91b
Lee Soe <LSoe@BitTitan.com>
2017-10-16 13:59:34 -0700

    Modify init and add sample scripts for operations on custom properties

---
+++ PowerShell/BuildingBlocks/CustomerAndResources/RetrieveCustomProperty.ps1
@@ -0,0 +1,13 @@
+﻿# Initialize the session
+.\Init.ps1
+
+# Retrieve a customer and create an extended property associated to the customer
+$customer = Get-BT_Customer -Ticket $mspc.Ticket -CompanyName "Default"
+$extendedProperty = Add-BT_ExtendedProperty -Ticket $mspc.WorkgroupTicket -Name "ExampleProperty" -Value "Custom Value" -ReferenceEntityType "Customer" -ReferenceEntityId $customer.Id
+
+# Retrieve the updated customer entity
+$customer = Get-BT_Customer -Ticket $mspc.Ticket -CompanyName "Default"
+Write-Output "The value of the custom property with name 'DefaultCustomProperty' is '$($customer.ExtendedProperties.ExampleProperty)'."
+
+# Delete the created custom property
+Remove-BT_ExtendedProperty -Ticket $mspc.WorkgroupTicket -Id $extendedProperty.Id -Force
---
+++ PowerShell/BuildingBlocks/CustomerAndResources/UpdateCustomProperty.ps1
@@ -0,0 +1,15 @@
+﻿# Initialize the session
+.\Init.ps1
+
+# Retrieve a customer and create an extended property associated to the customer
+$customer = Get-BT_Customer -Ticket $mspc.Ticket -CompanyName "Default"
+$extendedProperty = Add-BT_ExtendedProperty -Ticket $mspc.WorkgroupTicket -Name "ExampleProperty" -Value "Custom Value" -ReferenceEntityType "Customer" -ReferenceEntityId $customer.Id
+Write-Output "Old value is: $($extendedProperty.Value)."
+
+# Update the custom property
+$extendedProperty.Value = "New Custom Value"
+$extendedProperty = Set-BT_ExtendedProperty -Ticket $mspc.WorkgroupTicket -extendedproperty $extendedProperty
+Write-Output "New value is: $($extendedProperty.Value)."
+
+# Delete the created custom property
+Remove-BT_ExtendedProperty -Ticket $mspc.WorkgroupTicket -Id $extendedProperty.Id -Force
