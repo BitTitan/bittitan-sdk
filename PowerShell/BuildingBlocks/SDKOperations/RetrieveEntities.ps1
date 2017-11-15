8e05247393b5ebb3e6aa64c559f28132444738b9
Shutao Xu <SXu@BitTitan.com>
2017-11-15 09:55:47 +0800

    used to use endpoints

--- PowerShell/BuildingBlocks/SDKOperations/RetrieveEntities.ps1
+++ PowerShell/BuildingBlocks/SDKOperations/RetrieveEntities.ps1
@@ -1,39 +1,40 @@
 # Initialize the context
-.\Init.ps1
+# Create a customer scoped ticket
+.\Init.ps1 -Customer
 
 # Here are 3 common ways to retrieve entities
-# 1. Retrieve all the customers in a workgroup and process them one by one
+# 1. Retrieve all the endpoints under a customer and process them one by one
 # Use -RetrieveAll and piping with ForEach 
-Get-BT_Customer -Ticket $mspc.WorkgroupTicket -RetrieveAll | ForEach {
-    # Process each customer retrieved
-    Write-Host $_.CompanyName
+Get-BT_Endpoint -Ticket $mspc.CustomerTicket -RetrieveAll | ForEach {
+    # Process each endpoint retrieved
+    Write-Host $_.Name
 }
 
-# 2. Retrieve and process customers from a workgroup in batches
+# 2. Retrieve and process endpoints under a customer in batches
 # Use paging
 $pageSize = 100
 $count = 0
 While( $true ) {   
-    # Retrieve customers in batch
-    [array]$batch = Get-BT_Customer -Ticket $mspc.WorkgroupTicket -PageOffset $($count * $pageSize)
+    # Retrieve endpoints in batch
+    [array]$batch = Get-BT_Endpoint -Ticket $mspc.CustomerTicket -PageOffset $($count * $pageSize)
     if ( -not $batch ) { 
         break
     }
     
-    # Update each customer in the batch
-    ForEach($customer in $batch) {
-        $customer.CountryName = 'USA'
+    # Update each endpoint in the batch
+    ForEach($endpoint in $batch) {
+        $endpoint.Name += "_test"
     }
 
     # Send update request
-    Set-BT_Customer -Ticket $mspc.WorkgroupTicket -Customer $batch
+    Set-BT_Endpoint -Ticket $mspc.CustomerTicket -Endpoint $batch
 
     # Increase count
     $count ++
 }
 
-# 3. Retrieve all the customers in a workgroup to get certain info, e.g. count 
+# 3. Retrieve all the endpoints under a customer to get certain info, e.g. count 
 # Use -RetrieveAll
 # Note: This is the least efficient way among the 3 cases since it loads all the entities into the memory
-$customers = Get-BT_Customer -Ticket $mspc.WorkgroupTicket -RetrieveAll
-$customers.Count
+$endpoints = Get-BT_Endpoint -Ticket $mspc.CustomerTicket -RetrieveAll
+$endpoints.Count
