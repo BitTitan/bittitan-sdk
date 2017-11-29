5a98e91dd2986776c792614cedca883e1f0e4839
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2017-11-29 10:12:05 -0800

    Add MigrationWiz sample scripts

---
+++ PowerShell/BuildingBlocks/MigrationWiz/CreateProject.ps1
@@ -0,0 +1,28 @@
+# This script illustrates how to create a project in MigrationWiz
+
+# Authenticate
+$creds = Get-Credential -Message "Enter BitTitan credentials"
+$ticket = Get-BT_Ticket -Credentials $creds -ServiceType BitTitan
+$mwTicket = Get-MW_Ticket -Credentials $creds
+
+# Retrieve a customer, in order to create the project under that customer 
+$customer = Get-BT_Customer -Ticket $ticket -CompanyName "Default"
+
+# In this example, we create a mailbox project from Exchange Server to O365
+# Set up export and import configurations
+$exportConfiguration = New-Object -TypeName MigrationProxy.WebApi.ExchangeConfiguration -Property @{
+        "UseAdministrativeCredentials" = $false;
+        "Url" = "https://exchange-server-url.com";
+        "ExchangeItemType" = "Mail";
+}
+# You can choose to provide admin credentials and set UseAdministrativeCredentials to true, then you do not need to provide usernames and passwords in mailbox creation.
+
+$importConfiguration = New-Object -TypeName MigrationProxy.WebApi.ExchangeConfiguration -Property @{
+        "UseAdministrativeCredentials" = $false;
+        "ExchangeItemType" = "Mail";
+}
+
+# Create a new project
+$connector = Add-MW_MailboxConnector -Ticket $mwTicket -ProjectType Mailbox -ExportType ExchangeServer -ImportType Office365 `
+    -Name "TestProject" -UserId $mwTicket.UserId -OrganizationId $customer.OrganizationId `
+    -ExportConfiguration $exportConfiguration -ImportConfiguration $importConfiguration
---
+++ PowerShell/BuildingBlocks/MigrationWiz/StartMigration.ps1
@@ -0,0 +1,18 @@
+﻿# This script illustrates how to start migrations
+# This assumes that the migration items have corresponding subscriptions or licenses
+# Examples of how to assign subscriptions can be found in our GitHub sample scripts
+
+# Authenticate
+$creds = Get-Credential -Message "Enter BitTitan credentials"
+$mwTicket = Get-MW_Ticket -Credentials $creds
+
+# Retrieve an existing project
+$connector = Get-MW_MailboxConnector -Ticket $mwTicket -Name "TestProject"
+
+# Retrieve all the migration items in the project
+$mailboxes = Get-MW_Mailbox -Ticket $mwTicket -ConnectorId $connector.Id -RetrieveAll | ForEach {
+    # Start a migration for each item
+    # -Type indicates the type of migration, e.g. Trial, Full, etc.
+    # -ItemTypes indicates what item types to migrate, e.g. Contact, Calendar, etc. If not specified, all item types will be migrated.
+    $migration = Add-MW_MailboxMigration -Ticket $mwTicket -ConnectorId $connector.Id -MailboxId $_.Id -UserId $mwTicket.UserId -Type Full -ItemTypes Contact   
+}
