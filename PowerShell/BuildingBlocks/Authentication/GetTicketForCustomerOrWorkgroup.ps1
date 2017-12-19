12894c7338e1bdbaa5d30081b4ce05115761cb57
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2017-12-18 18:00:59 -0800

    Add sample scripts for authentication

---
+++ PowerShell/BuildingBlocks/Authentication/GetTicketForCustomerOrWorkgroup.ps1
@@ -0,0 +1,17 @@
+﻿# This script shows how to get an authentication ticket
+# 1) Scoped to a customer
+# 2) Scoped to a workgroup
+
+# Authenticate
+$creds = Get-Credential
+$ticket = Get-BT_Ticket -Credentials $credentials -ServiceType BitTitan
+
+# 1) Get a ticket for a customer
+$customerId = [GUID](Read-Host -Prompt 'Customer ID')  
+$customer = Get-BT_Customer -Ticket $ticket -Id $customerId
+$customerTicket = Get-BT_Ticket -Ticket $ticket -OrganizationId $customer.OrganizationId
+
+# 2) Get a ticket for a workgroup
+$workgroupId = [GUID](Read-Host -Prompt 'Workgroup ID')  
+$workgroup = Get-BT_Workgroup -Ticket $ticket -Id $workgroupId
+$workgroupTicket = Get-BT_Ticket -Ticket $ticket -OrganizationId $workgroup.WorkgroupOrganizationId
---
+++ PowerShell/BuildingBlocks/Authentication/SetDefaultTicket.ps1
@@ -0,0 +1,9 @@
+﻿# This script shows how to set a ticket to be used by default
+
+# Get a ticket and set it as default
+$creds = Get-Credential
+Get-BT_Ticket -Credentials $creds -ServiceType BitTitan -SetDefault
+
+# Retrieve customers without specifying the ticket
+# This will use the ticket previously set as default
+$customers = Get-BT_Customer
