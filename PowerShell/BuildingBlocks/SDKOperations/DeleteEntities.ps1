d629729ff357fa8309c51006bb512d5bd680d7e0
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2017-12-19 11:42:19 -0800

    Add sample scripts for requests

---
+++ PowerShell/BuildingBlocks/SDKOperations/DeleteEntities.ps1
@@ -0,0 +1,17 @@
+﻿# This script illustrates how to delete entities
+
+# Authenticate
+$creds = Get-Credential
+$ticket = Get-BT_Ticket -Credentials $creds -ServiceType BitTitan
+
+# Get a customer ticket
+$customer = Get-BT_Customer -Ticket $ticket -CompanyName "Default"
+$customerTicket = Get-BT_Ticket -Ticket $ticket -OrganizationId $customer.OrganizationId
+
+# Retrieve and delete a single endpoint under that customer
+$endpoint = Get-BT_Endpoint -Ticket $customerTicket -Name "Test Endpoint" -IsDeleted false
+Remove-BT_Endpoint -Ticket $customerTicket -Id $endpoint.Id
+
+# Retrieve and delete multiple endpoints under that customer
+$endpoints = Get-BT_Endpoint -Ticket $customerTicket -Name @("Endpoint 1", "Endpoint 2") -IsDeleted false
+Remove-BT_Endpoint -Ticket $customerTicket -Id $endpoints.Id
---
+++ PowerShell/BuildingBlocks/SDKOperations/UpdateEntities.ps1
@@ -0,0 +1,27 @@
+﻿# This script illustrates how to update entities
+
+# Authenticate
+$creds = Get-Credential
+$ticket = Get-BT_Ticket -Credentials $creds -ServiceType BitTitan
+
+# Get a customer ticket
+$customer = Get-BT_Customer -Ticket $ticket -CompanyName "Default"
+$customerTicket = Get-BT_Ticket -Ticket $ticket -OrganizationId $customer.OrganizationId
+
+# Retrieve and update a single endpoint
+$endpoint = Get-BT_Endpoint -Ticket $customerTicket -Name "Endpoint1" -IsDeleted false
+$endpoint = Set-BT_Endpoint -Ticket $customerTicket -Endpoint $endpoint -Name "Endpoint1 updated"
+Write-Output $endpoint
+
+# Alternatively, the endpoint's properties can also be modified directly on the object
+$endpoint.Name = "Endpoint1 updated again"
+$endpoint = Set-BT_Endpoint -Ticket $customerTicket -Endpoint $endpoint
+Write-Output $endpoint
+
+# Retrieve and update multiple endpoints
+$endpoints = Get-BT_Endpoint -Ticket $customerTicket -Name @("Endpoint2", "Endpoint3") -IsDeleted false
+$endpoints | ForEach {
+    $_.Name += " updated"
+}
+$endpoints = Set-BT_Endpoint -Ticket $customerTicket -Endpoint $endpoints
+Write-Output $endpoints
