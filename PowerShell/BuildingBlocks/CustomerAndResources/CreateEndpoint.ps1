e6a72175f779247169f9147aa8b6270ff2729da9
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2017-12-22 11:58:36 -0800

    Fix comment

--- PowerShell/BuildingBlocks/CustomerAndResources/CreateEndpoint.ps1
+++ PowerShell/BuildingBlocks/CustomerAndResources/CreateEndpoint.ps1
@@ -9,7 +9,7 @@ $customerId = [GUID](Read-Host -Prompt 'Customer ID')
 $customer = Get-BT_Customer -Ticket $ticket -Id $customerId
 $customerTicket = Get-BT_Ticket -Ticket $ticket -OrganizationId $customer.OrganizationId
 
-# Initialize a configuration (in this case and Exchange configuration)
+# Initialize a configuration (in this case an Exchange configuration)
 $endpointConfiguraton = New-BT_ExchangeConfiguration -AdministrativeUsername "TestUserName" -AdministrativePassword "TestPassword" -UseAdministrativeCredentials $True -ExchangeItemType Mail
 
 # Create a new endpoint
