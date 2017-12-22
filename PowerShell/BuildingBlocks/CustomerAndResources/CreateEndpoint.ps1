472ab1bfebf3a9f9e9969ff1360ac4c5fce03f49
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2017-12-22 11:55:57 -0800

    Endpoints, end users, feeds and reports

---
+++ PowerShell/BuildingBlocks/CustomerAndResources/CreateEndpoint.ps1
@@ -0,0 +1,17 @@
+﻿# This script illustrates how to create an endpoint
+
+# Authenticate
+$creds = Get-Credential
+$ticket = Get-BT_Ticket -Credentials $creds -ServiceType BitTitan
+
+# Retrieve a customer and get a ticket for it
+$customerId = [GUID](Read-Host -Prompt 'Customer ID')    
+$customer = Get-BT_Customer -Ticket $ticket -Id $customerId
+$customerTicket = Get-BT_Ticket -Ticket $ticket -OrganizationId $customer.OrganizationId
+
+# Initialize a configuration (in this case and Exchange configuration)
+$endpointConfiguraton = New-BT_ExchangeConfiguration -AdministrativeUsername "TestUserName" -AdministrativePassword "TestPassword" -UseAdministrativeCredentials $True -ExchangeItemType Mail
+
+# Create a new endpoint
+$endpoint = Add-BT_Endpoint -Ticket $customerTicket -Configuration $endpointConfiguraton -Type ExchangeOnline2 -Name "Test Endpoint"
+Write-Output $endpoint
---
+++ PowerShell/BuildingBlocks/CustomerAndResources/RetrieveReport.ps1
@@ -0,0 +1,15 @@
+﻿# This script illustrates how to retrieve report data
+
+# Authenticate
+$creds = Get-Credential
+$ticket = Get-BT_Ticket -Credentials $creds -ServiceType BitTitan
+
+# Retrieve a workgroup and get a ticket for it
+$workgroupId = [GUID](Read-Host -Prompt 'Workgroup ID')    
+$workgroup = Get-BT_Workgroup -Ticket $ticket -Id $workgroupId
+$workgroupTicket = Get-BT_Ticket -Ticket $ticket -OrganizationId $workgroup.WorkgroupOrganizationId
+
+# Retrieve the last generated report and its data.
+$reportInstance = Get-BT_ReportInstance -Ticket $workgroupTicket -PageSize 1 -SortBy_GeneratedOn_Descending
+$reportWithData = Get-BT_ReportWithData -Ticket $workgroupTicket -ReportInstanceId $reportInstance.Id
+Write-Output $reportWithData
