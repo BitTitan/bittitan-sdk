7f67ee96eb88899f0f5bfd3c07debb6def39d83a
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2018-01-04 09:42:37 -0800

    Add credentials logic

--- PowerShell/BuildingBlocks/SDKOperations/DependentEntityFiltering.ps1
+++ PowerShell/BuildingBlocks/SDKOperations/DependentEntityFiltering.ps1
@@ -1,5 +1,9 @@
 ﻿# This script illustrates how to retrieve entities with dependent entity filtering
 
+# Authenticate
+$creds = Get-Credential -Message "Enter BitTitan credentials"
+$ticket = Get-BT_Ticket -Credentials $creds -ServiceType BitTitan
+
 # To get a workgroup that has a customer named BitTitan
 $workgroup = Get-BT_Workgroup -Ticket $ticket -CustomerCompanyName 'BitTitan'
 
